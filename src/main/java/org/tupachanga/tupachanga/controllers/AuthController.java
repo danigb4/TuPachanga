3c00810fb0012c9681ccbbfaa879d1e06bd92548
danigb4 <dani.estudiosdaw@gmail.com>
2025-06-14 17:52:25 +0200

    add(navbar link): access to show joinrequest in all views

--- src/main/java/org/tupachanga/tupachanga/controllers/AuthController.java
+++ src/main/java/org/tupachanga/tupachanga/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package org.tupachanga.tupachanga.controllers;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
